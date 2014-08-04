d53ec8193587cdc3a7248163a780cbbce36c2422
precode <info@precode.io>
2014-08-04 14:16:57 -0500

    Updated menu and header styling

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'tcrs' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'tcrs' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'tcrs' ), 'tcrs', '<a href="http://precode.io/" rel="designer">Precode</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'tcrs' ), 'Two Column Right Sidebar', '<a href="http://precode.io/" rel="designer">Precode</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
